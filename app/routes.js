d7f4a00819b854e0faab6a89c2a88b403d88fd38
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-03-25 10:40:23 +0000

    Add you need legal authority page

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,14 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/legal-authority-answer', function (req, res) {
+  let legalAuthority = req.session.data['legal-authority']
+
+  if (legalAuthority === 'yes') {
+    res.redirect('/agreement')
+  } else {
+    res.redirect('/no-legal-authority')
+  }
+})
+
 module.exports = router
